3f0232ab46c28bba2f1f4916fec85f23141ff766
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-12 14:33:45 +0300

    Solved The Contains Duplicate problem in leet code with time complexity of O(n)

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ContainsDuplicate
+{
+    public ContainsDuplicate() { }
+
+    public bool Run(int[] nums)
+    {
+        Dictionary<int, int> seen = new();
+        for (int i = 0; i < nums.Length; i++) {
+            if (seen.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+
+            seen.Add(nums[i], i);
+        }
+            return false;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class DSA
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 3 };
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        Console.WriteLine(containsDuplicate.Run(nums));
+    }
+}
