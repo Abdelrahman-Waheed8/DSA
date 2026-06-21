25366f8e4e9005da3dc13f7b47b9a3bb2b9f3f86
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-21 17:54:15 +0300

    Solved IsAnagram problem with time complexity of O(N)

---
+++ Anagram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Anagram
+{
+    public Anagram() { }
+
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        Dictionary<char, int> firstString = new();
+        Dictionary<char, int> secondString = new();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (firstString.ContainsKey(s[i]))
+            {
+                firstString[s[i]]++;
+            }
+            else
+            {
+                firstString.Add(s[i], 1);
+            }
+
+            if (secondString.ContainsKey(t[i]))
+            {
+                secondString[t[i]]++;
+            }
+            else
+            {
+                secondString.Add(t[i], 1);
+            }
+        }
+
+        foreach(var pair in firstString)
+        {
+            if(!secondString.TryGetValue(pair.Key, out int count) || pair.Value != count)
+            {
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,15 @@ public class DSA
 {
     public static void Main(string[] args)
     {
-        int[] nums = { 1, 2, 3, 3 };
-        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        Console.WriteLine(containsDuplicate.Run(nums));
+        // CONTAINS DUPLICATE 
+        //int[] nums = { 1, 2, 3, 3 };
+        //ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        //Console.WriteLine(containsDuplicate.Run(nums));
+
+        Anagram anagram = new Anagram();
+        string a = "jar";
+        string b = "jam";
+        Console.WriteLine(anagram.IsAnagram(a, b));
+
     }
 }
