936b20260fcf0c125c44c356a82eb9cabf46071e
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-22 19:29:44 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> hashmap = new();
+        char[] sorted ;
+        List<List<string>> result = new();
+        List<string> anagrams = new();
+        List<string> sortedstrings = [];
+
+        for(int i = 0; i< strs.Length; i++)
+        {
+            sorted = strs[i].ToCharArray();
+            Array.Sort(sorted);
+            sortedstrings.Add(new string(sorted));
+            if(hashmap.ContainsKey(sortedstrings[i]))
+            {
+                hashmap[sortedstrings[i]].Add(strs[i]);
+            }
+            else
+            {
+                hashmap.Add(sortedstrings[i], new List<string> { strs[i]});
+            }
+        }
+
+        foreach(var kvp in hashmap)
+        {
+            result.Add(kvp.Value);
+        }
+        return result;
+    }
+}
