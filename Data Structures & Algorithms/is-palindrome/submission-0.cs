e762090fbec754f5774a2492b7db4f0e7b34a184
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-06-24 18:31:02 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if (string.IsNullOrEmpty(s)) return true;
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right) {
+            // Move the left pointer forward if the character is not alphanumeric
+            if (!char.IsLetterOrDigit(s[left])) {
+                left++;
+            }
+            // Move the right pointer backward if the character is not alphanumeric
+            else if (!char.IsLetterOrDigit(s[right])) {
+                right--;
+            }
+            // Both characters are alphanumeric; compare them case-insensitively
+            else {
+                if (char.ToLowerInvariant(s[left]) != char.ToLowerInvariant(s[right])) {
+                    return false; // Mismatch found
+                }
+                left++;
+                right--;
+            }
+        }
+
+        return true; // All valid pairs matched
+    }
+}
